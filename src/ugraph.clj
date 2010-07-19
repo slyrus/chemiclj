bfb90cbb5589fa7fdb99ebe8f1df3e9dc269bb7a
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-07-18 18:31:54 -0700

    more protocol refactoring, dfs/bfs work  * rename protocols, added arcs and related  * UndirectedEdge -> Edge  * added breadth-first-traversal-with-path and depth-...

--- src/ugraph.clj
+++ src/ugraph.clj
@@ -1,52 +1,94 @@
+;;; file: ugraph.clj
+;;;
+;;; Copyright (c) 2010 Cyrus Harmon (ch-lisp@bobobeach.com) All rights
+;;; reserved.
+;;;
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions
+;;; are met:
+;;;
+;;;   * Redistributions of source code must retain the above copyright
+;;;     notice, this list of conditions and the following disclaimer.
+;;;
+;;;   * Redistributions in binary form must reproduce the above
+;;;     copyright notice, this list of conditions and the following
+;;;     disclaimer in the documentation and/or other materials
+;;;     provided with the distribution.
+;;;
+;;; THIS SOFTWARE IS PROVIDED BY THE AUTHOR 'AS IS' AND ANY EXPRESSED
+;;; OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+;;; DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+;;; GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+;;; WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 (ns ugraph)
 
 (defprotocol NodeSet
   (nodes [graph])
-  (node? [graph node]))
-
-(defprotocol Graph
+  (node? [graph node])
   (add-node [graph node])
-  (add-edge [graph node1 node2]
-            [graph node1 node2 object])
+  (neighbors [graph node]))
+
+(defprotocol EdgeSet
   (edges [graph]
          [graph node])
   (edge? [graph node1 node2])
-  (neighbors [graph node]))
+  (add-edge [graph node1 node2]
+            [graph node1 node2 object]))
 
-(defprotocol Edge
-  (other-node [edge y]))
+(defprotocol ArcInGraph
+  (start [edge])
+  (end [edge]))
 
-(defrecord UndirectedEdge [node1 node2]
+(defrecord Edge [node1 node2]
   NodeSet
   (nodes [edge] (vector node1 node2))
   (node? [edge node] (or (= node node1) (= node node2)))
+  (neighbors [edge node] (list (cond
+                                (= node node1) node2
+                                (= node node2) node1))))
 
-  Edge
-  (other-node [edge node] (cond
-                           (= node node1) node2
-                           (= node node2) node1)))
+(defrecord Arc [start-node end-node]
+  NodeSet
+  (nodes [edge] (vector start-node end-node))
+  (node? [edge node] (or (= node start-node) (= node end-node)))
+  (neighbors [edge node] (list (cond
+                                (= node start-node) end-node
+                                (= node end-node) start-node)))
+  ArcInGraph
+  (start [edge] start-node)
+  (end [edge] end-node))
 
 ;;; forward declaration for make-ugraph which we can't defn until we
-;;; defrecord UndirectedGraph
+;;; defrecord Graph
 (declare make-ugraph)
 
-(defrecord UndirectedGraph [node-set edge-map]
+(defrecord Graph [node-set edge-map]
   NodeSet
   (nodes [g] (:node-set g))
   (node? [g node] (get (nodes g) node))
-  Graph
   (add-node [g n]
             (make-ugraph (conj (:node-set g) n) (:edge-map g)))
+  (neighbors [g node]
+             (map #(first (neighbors % node)) (vals (get (:edge-map g) node))))
+  
+  EdgeSet
   (edges [g]
          (distinct (apply concat (map vals (vals (:edge-map g))))))
   (edges [g node]
          (vals (get (:edge-map g) node)))
+  
   (edge? [g n1 n2]
          (some #(when (node? % n2) %)
                (vals (get (:edge-map g) n1))))
   (add-edge [g n1 n2]
-            (add-edge g n1 n2 (UndirectedEdge. n1 n2)))
+            (add-edge g n1 n2 (Edge. n1 n2)))
   (add-edge [g n1 n2 obj]
             (letfn [(add-1-edge [e n1 n2 obj]
                                 (assoc e n1 (assoc (or (get e n1) {}) n2 obj)))]
@@ -55,14 +97,12 @@
                 (make-ugraph (:node-set g)
                              (add-1-edge
                               (add-1-edge (:edge-map g) n2 n1 obj)
-                              n1 n2 obj)))))
-  (neighbors [g node]
-             (map #(other-node % node) (vals (get (:edge-map g) node)))))
+                              n1 n2 obj))))))
 
 (defn make-ugraph
-  ([] (UndirectedGraph. #{} {}))
-  ([nodes] (UndirectedGraph. nodes {}))
-  ([nodes edges] (UndirectedGraph. nodes edges)))
+  ([] (Graph. #{} {}))
+  ([nodes] (Graph. nodes {}))
+  ([nodes edges] (Graph. nodes edges)))
 
 (defn add-edges [g edge-vec]
   (reduce (fn [g [n1 n2]] (add-edge g n1 n2)) g edge-vec))
@@ -83,6 +123,26 @@
                 (breadth-first-traversal g (into (subvec queue 1) next)
                                          (into (conj visited node) next))))))))
 
+;;; often it's nice to not just do a search, but keep a trail of the
+;;; path of how one got to a particular node. we're also going to want
+;;; to know the distance from the start, so we can just take the
+;;; length of the path to get that.
+(defn breadth-first-traversal-with-path
+  ([g start]
+     (when (node? g start)
+       (breadth-first-traversal-with-path g [[start]] #{})))
+  ([g queue visited]
+     (lazy-seq
+      (if (seq queue)
+        (let [node (last (first queue))
+              next (remove visited (neighbors g node))]
+          (cons (first queue)
+                (breadth-first-traversal-with-path
+                  g
+                  (into (subvec queue 1)
+                        (vec (map #(conj (first queue) %) (vec next))))
+                  (into (conj visited node) next))))))))
+
 (defn depth-first-traversal
   ([g start]
      (when (node? g start)
@@ -92,9 +152,31 @@
       (if (seq queue)
         (let [node (first queue)
               next (remove visited (neighbors g node))]
-          (cons node
-                (depth-first-traversal g (into (rest queue) next)
-                                       (into (conj visited node) next))))))))
+          (if-not (visited node)
+            (cons node
+                  (depth-first-traversal g (into (rest queue) next)
+                                         (conj visited node)))
+            (depth-first-traversal g (into (rest queue) next) visited)))))))
+
+(defn depth-first-traversal-with-path
+  ([g start]
+     (when (node? g start)
+       (depth-first-traversal-with-path g (list [start]) #{})))
+  ([g queue visited]
+     (lazy-seq
+      (if (seq queue)
+        (let [node (last (first queue))
+              next (remove visited (neighbors g node))]
+          (if-not (visited node)
+            (cons (first queue)
+                  (depth-first-traversal-with-path g
+                    (into (rest queue)
+                          (vec (map #(conj (first queue) %) (vec next))))
+                    (conj visited node)))
+            (depth-first-traversal-with-path g
+                        (into (rest queue)
+                              (vec (map #(conj (first queue) %) (vec next))))
+                        visited)))))))
 
 
 ;;; scratch
