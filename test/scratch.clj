5dd329230d3bdf38657157e0654cd2f8679cccd3
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-07-24 10:25:23 -0700

    :use chemiclj.element and new chemiclj API calls instead of assembling things by hand

--- test/scratch.clj
+++ test/scratch.clj
@@ -1,10 +1,9 @@
 
 (ns chemiclj-test
   (:use chemiclj.core)
-  (:use shortcut.graph)
-  
-  (:import chemiclj.core.Atom)
-  (:import shortcut.graph.Graph))
+  (:use chemiclj.element)
+
+  (:use shortcut.graph))
 
 (def c1 (make-atom :c "C1"))
 (def h1 (make-atom :h "H1"))
@@ -15,11 +14,11 @@
 (def h5 (make-atom :h "H5"))
 (def h6 (make-atom :h "H6"))
 
-(def ethane (add-edges (Graph. #{c1 h1 h2 h3 c2 h4 h5 h6} {})
-                       [[c1 h1]
-                        [c1 h2]
-                        [c1 h3]
-                        [c1 c2]
-                        [c2 h4]
-                        [c2 h5]
-                        [c2 h6]]))
+(def ethane (make-molecule #{c1 h1 h2 h3 c2 h4 h5 h6}
+                           [[c1 h1]
+                            [c1 h2]
+                            [c1 h3]
+                            [c1 c2]
+                            [c2 h4]
+                            [c2 h5]
+                            [c2 h6]]))
