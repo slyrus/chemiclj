91778a693a19ad6dea1f38c8e49560d3acf69b23
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-07-28 10:00:56 -0700

    remove outdated dependencies and add a README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+
+chemiclj
+Thu Jul 22 00:02:05 2010
+
+Cyrus Harmon
+ch-lisp at bobobeach.com
+
+chemiclj is a chemistry library for clojure
+
+NOTE: make sure that the dependencies are installed be lein deps, or by
+putting the appropriate symlinks in a directory named checkouts.
+
+Dependencies
+
+ shortuct - a graph library for clojure
+
+currently, chemiclj only requires shortcut (and clojure and
+clojure-contrib) but as there is (not yet) an official release of a
+shortcut jar on clojars, or elsewhere, one MUST INSTALL SHORTCUT BY
+HAND IN PLACE A SYMLINK TO IT IN THE checkouts DIRECTORY. This
+requires leiningen 1.2 or later.
--- project.clj
+++ project.clj
@@ -2,11 +2,6 @@
 (defproject chemiclj "0.0.0"
   :description "A computational chemistry library in clojure"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [org.freehep/freehep-graphics2d "2.1.1"]
-                 [org.freehep/freehep-graphicsio-pdf "2.1.1"]
-                 [org.freehep/freehep-graphicsio-svg "2.1.1"]
-                 [hiccup "0.2.6"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
-  :repositories {"freehep" "http://java.freehep.org/maven2"})
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
 
