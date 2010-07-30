1957c10fdfaa50409b5e8cbda98701d3e347521a
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-07-30 13:21:53 -0700

    checkpoint on smiles parsing. adding support for making chemicl.core/Atoms. not done yet.

--- src/chemiclj/smiles2.clj
+++ src/chemiclj/smiles2.clj
@@ -6,6 +6,17 @@
             [clojure.contrib [except :as except]])
   (:refer-clojure :exclude #{read-string}))
 
+(defrecord SMILESContext
+  [molecule last-atom atom-counts])
+
+(defn get-atom-count [context element]
+  (or (get (:atom-counts context) element) 0))
+
+(defn inc-atom-count [context element]
+  (assoc context :atom-counts
+         (assoc (:atom-counts context) element
+                (inc (get-atom-count context element)))))
+
 (defrecord BracketAtom
   [isotope symbol chirality hydrogen-count charge class])
 
@@ -15,13 +26,13 @@
 (h/defrule <left-bracket> (h/lit \[))
 (h/defrule <right-bracket> (h/lit \]))
 
-(h/defrule <aliphatic-element-symbol>
+(h/defrule <bracket-aliphatic-element-symbol>
   (h/hook (comp str* concat)
           (h/cat h/<uppercase-ascii-letter>
                  (h/opt h/<lowercase-ascii-letter>)
                  (h/opt h/<lowercase-ascii-letter>))))
 
-(h/defrule <aromatic-element-symbol>
+(h/defrule <bracket-aromatic-element-symbol>
   (h/+ (h/hook (comp str* concat) (h/cat (h/lit \s) (h/lit \e)))
        (h/hook (comp str* concat) (h/cat (h/lit \a) (h/lit \s)))
        (h/hook str (h/lit \c))
@@ -30,26 +41,52 @@
        (h/hook str (h/lit \p))
        (h/hook str (h/lit \s))))
 
-(h/defrule <chlorine> (h/hook (comp str* concat)
+(h/defrule <bracket-element-symbol>
+  (h/label "a bracket element symbol"
+           (h/+ <bracket-aliphatic-element-symbol> <bracket-aromatic-element-symbol>)))
+
+(h/defrule <aliphatic-chlorine> (h/hook (comp str* concat)
                               (h/cat (h/lex (h/lit \C)) (h/lit \l))))
-(h/defrule <bromine> (h/hook (comp str* concat)
+(h/defrule <aliphatic-bromine> (h/hook (comp str* concat)
                              (h/cat (h/lex (h/lit \B)) (h/lit \r))))
-(h/defrule <boron> (h/hook str (h/lit \B)))
-(h/defrule <carbon> (h/hook str (h/lit \C)))
-(h/defrule <nitrogen> (h/hook str (h/lit \N)))
-(h/defrule <oxygen> (h/hook str (h/lit \O)))
-(h/defrule <sulfur> (h/hook str (h/lit \S)))
-(h/defrule <phosphorus> (h/hook str (h/lit \P)))
-(h/defrule <fluorine> (h/hook str (h/lit \F)))
-(h/defrule <iodine> (h/hook str (h/lit \I)))
+(h/defrule <aliphatic-boron> (h/hook str (h/lit \B)))
+(h/defrule <aliphatic-carbon> (h/hook str (h/lit \C)))
+(h/defrule <aliphatic-nitrogen> (h/hook str (h/lit \N)))
+(h/defrule <aliphatic-oxygen> (h/hook str (h/lit \O)))
+(h/defrule <aliphatic-sulfur> (h/hook str (h/lit \S)))
+(h/defrule <aliphatic-phosphorus> (h/hook str (h/lit \P)))
+(h/defrule <aliphatic-fluorine> (h/hook str (h/lit \F)))
+(h/defrule <aliphatic-iodine> (h/hook str (h/lit \I)))
+
+(h/defrule <aliphatic-organic>
+  (h/+ <aliphatic-chlorine>
+       <aliphatic-bromine>
+       <aliphatic-boron>
+       <aliphatic-carbon>
+       <aliphatic-nitrogen>
+       <aliphatic-oxygen>
+       <aliphatic-sulfur>
+       <aliphatic-phosphorus>
+       <aliphatic-fluorine>
+       <aliphatic-iodine>))
+
+(h/defrule <aromatic-boron> (h/hook str (h/lit \b)))
+(h/defrule <aromatic-carbon> (h/hook str (h/lit \c)))
+(h/defrule <aromatic-nitrogen> (h/hook str (h/lit \n)))
+(h/defrule <aromatic-oxygen> (h/hook str (h/lit \o)))
+(h/defrule <aromatic-sulfur> (h/hook str (h/lit \s)))
+(h/defrule <aromatic-phosphorus> (h/hook str (h/lit \p)))
+
+(h/defrule <aromatic-organic>
+  (h/+ <aromatic-boron>
+       <aromatic-carbon>
+       <aromatic-nitrogen>
+       <aromatic-oxygen>
+       <aromatic-sulfur>
+       <aromatic-phosphorus>))
 
 (h/defrule <organic-subset-atom>
-  (h/+ <chlorine> <bromine> <boron> <carbon> <nitrogen> <oxygen> <sulfur>
-       <phosphorus> <fluorine> <iodine>))
-
-(h/defrule <element-symbol>
-  (h/label "an element symbol"
-           (h/+ <aliphatic-element-symbol> <aromatic-element-symbol>)))
+  (h/+ <aliphatic-organic> <aromatic-organic>))
 
 (h/defrule <bond>
   (h/+
@@ -105,22 +142,28 @@
             <charge>))))
 
 (h/defrule <bracket-expr>
-  (h/hook (fn [[isotope symbol {:keys #{hydrogens chirality charge}} class]]
-            (chemiclj.smiles2.BracketAtom.
-             isotope symbol chirality hydrogens charge class))
-          (h/label "a bracket expression"
-                   (h/circumfix <left-bracket>
-                                (h/cat
-                                 (h/opt <isotope>)
-                                 <element-symbol>
-                                 <bracket-mods>
-                                 (h/opt
-                                  (h/prefix (h/lit \:)
-                                            <decimal-natural-number>)))
-                                <right-bracket>))))
+  (h/for [context h/<fetch-context>
+          atom (h/hook (fn [[isotope symbol {:keys #{hydrogens chirality charge}} class]]
+                         (make-atom symbol (str symbol (inc (get-atom-count context symbol)))
+                                    isotope chirality charge nil hydrogens))
+                       (h/label "a bracket expression"
+                                (h/circumfix <left-bracket>
+                                             (h/cat
+                                              (h/opt <isotope>)
+                                              <element-symbol>
+                                              <bracket-mods>
+                                              (h/opt
+                                               (h/prefix (h/lit \:)
+                                                         <decimal-natural-number>)))
+                                             <right-bracket>)))]
+         atom))
 
 (h/defrule <atom>
-  (h/+ <organic-subset-atom> <bracket-expr>))
+  (h/for "an atom"
+         [atom (h/+ <organic-subset-atom> <bracket-expr>)
+          _ (h/alter-context inc-atom-count (-> atom :element :id))]
+         (do (print atom)
+             atom)))
 
 (h/defrule <ws?>
   "Consumes optional, ignored whitespace."
@@ -144,12 +187,14 @@
 (declare <chain>)
 
 (h/defrule <branch>
-  (h/cat
-   (h/circumfix (h/lit \()
-                (h/cat
-                 (h/opt (h/+ <bond> <dot>))
-                 <chain>)
-                (h/lit \)))))
+  (let [context (h/<fetch-context>)
+        branch-attachment (:last-atom context)]
+    (h/cat
+     (h/circumfix (h/lit \()
+                  (h/cat
+                   (h/opt (h/+ <bond> <dot>))
+                   <chain>)
+                  (h/lit \))))))
 
 (h/defrule <chain>
   (h/rep
@@ -157,9 +202,15 @@
           <branched-atom>)))
 
 (defn read-string [input]
-  (c/matches-seq
-   (h/make-state input)
-   <chain>
+  (h/match
+   (h/make-state input
+                 :context (SMILESContext. (make-molecule) nil nil))
+   (h/for
+    [chain <chain>
+     _ <ws?>
+     _ h/<end-of-input>
+     context h/<fetch-context>]
+    context)
    :success-fn (fn [product position]
                  product)
    :failure-fn (fn [error]
