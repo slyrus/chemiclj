59fdb4f755fbfb0d966644bddc91f2677971babd
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-07-30 00:27:17 -0700

    add the beginnings of an fnparse3-based parser

---
+++ src/chemiclj/smiles2.clj
@@ -0,0 +1,73 @@
+
+(ns chemiclj.smiles2
+  (:use [chemiclj.core])
+  (:require [shortcut.graph :as graph]
+            [edu.arizona.fnparse [hound :as h] [core :as c]]
+            [clojure.contrib [except :as except]])
+  (:refer-clojure :exclude #{read-string}))
+
+(defrecord BracketAtomContext
+  [isotope symbol chirality hydrogen-count charge class])
+
+(defn str* [objects]
+  (apply str objects))
+
+(h/defrule <left-bracket> (h/lit \[))
+(h/defrule <right-bracket> (h/lit \]))
+
+(h/defrule <aliphatic-element-symbol>
+  (h/hook (comp str* concat)
+          (h/cat h/<uppercase-ascii-letter>
+                 (h/opt h/<lowercase-ascii-letter>)
+                 (h/opt h/<lowercase-ascii-letter>))))
+
+(h/defrule <aromatic-element-symbol>
+  (h/+ (h/hook (comp str* concat) (h/cat (h/lit \s) (h/lit \e)))
+       (h/hook (comp str* concat) (h/cat (h/lit \a) (h/lit \s)))
+       (h/hook str (h/lit \c))
+       (h/hook str (h/lit \n))
+       (h/hook str (h/lit \o))
+       (h/hook str (h/lit \p))
+       (h/hook str (h/lit \s))))
+
+(h/defrule <element-symbol>
+  (h/label "an element symbol"
+           (h/+ <aliphatic-element-symbol> <aromatic-element-symbol>)))
+
+(h/defmaker radix-natural-number [core]
+  (h/hooked-rep #(+ (* core %1) %2) 0 (h/radix-digit core)))
+
+(h/defrule <decimal-natural-number>
+  (radix-natural-number 10))
+
+(h/defrule <isotope>
+  (h/label "an isotope" <decimal-natural-number>))
+
+(h/defrule <bracket-mods>
+  (h/rep*
+   (h/+
+    (h/cat (h/lit \H) (h/opt <decimal-natural-number>))
+    (h/+ (h/cat (h/lit \@)) (h/lit \@)
+         (h/lit \@))
+    (h/+ (h/cat (h/lit \+) (h/opt <decimal-natural-number>))
+         (h/cat (h/lit \-) (h/opt <decimal-natural-number>))))))
+
+(h/defrule <bracket-expr>
+  (h/label "a bracket expression"
+           (h/prefix <left-bracket>
+                     (h/suffix
+                      (h/cat
+                       (h/opt <isotope>)
+                       <element-symbol>
+                       <bracket-mods>)
+                      <right-bracket>))))
+
+(defn read-string [input]
+  (c/matches-seq
+   (h/make-state input)
+   <bracket-expr>
+   :success-fn (fn [product position]
+                 product)
+   :failure-fn (fn [error]
+                 (except/throwf "SMILES parsing error: %s"
+                                (h/format-parse-error error)))))
