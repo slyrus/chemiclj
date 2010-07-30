37d930cfc2d331b98e57465ccfc8eaa61be5edae
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-07-30 16:23:22 -0700

    add some jvm-opts

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :description "A computational chemistry library in clojure"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
+  :jvm-opts ["-XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m -Xmx1g"])
 
