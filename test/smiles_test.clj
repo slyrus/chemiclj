215e06c068f153da1550f4333283fef2e078fd1c
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-08-02 09:31:02 -0700

    add some more compounds

--- test/smiles_test.clj
+++ test/smiles_test.clj
@@ -35,12 +35,21 @@
      (reduce (fn [acc [name smiles]]
                (into acc {name (read-smiles-string smiles)}))
              {}
-             {"tamoxifen" "CCC(C1=CC=CC=C1)=C(C2=CC=CC=C2)C3=CC=C(OCCN(C)C)C=C3"
+             {"valine" "CC(C)C(C(=O)O)N"
+              "paroxetine" "C1CNCC(C1C2=CC=C(C=C2)F)COC3=CC4=C(C=C3)OCO4"
+              "tamoxifen" "CCC(C1=CC=CC=C1)=C(C2=CC=CC=C2)C3=CC=C(OCCN(C)C)C=C3"
               "anastrozole" "CC(C)(C#N)C1=CC(=CC(=C1)CN2C=NC=N2)C(C)(C)C#N"
               "acetominophen" "CC(=O)NC1=CC=C(C=C1)O"
               "morphine" "CN1CCC23C4C1CC5=C2C(=C(C=C5)O)OC3C(C=C4)O"
               "estradiol" "CC12CCC3C(C1CCC2O)CCC4=C3C=CC(=C4)O"
-              "dicyclohexyl" "C1CCCCC1C2CCCCC2"}))
+              "dicyclohexyl" "C1CCCCC1C2CCCCC2"
+              "spiro[5.5]undecane" "C12(CCCCC1)CCCCC2"
+              "benzene" "c1ccccc1"
+              "indane" "c1ccc2CCCc2c1"
+              "furan" "c1occc1"
+              "fluoroform" "C(F)(F)F"
+              "vanillin" "O=Cc1ccc(O)c(OC)c1"
+              "thiamin" "OCCc1c(C)[n+](=cs1)Cc2cnc(C)nc(N)2"}))
 
 (defn get-molecule [name]
   (get *molecules* name))
