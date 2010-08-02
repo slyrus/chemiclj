b66274b1ca1ae5899156685d690782088f2eb6a1
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-08-01 22:39:32 -0700

    update to clojure 1.2.0-RC1 in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 
 (defproject chemiclj "0.0.0"
   :description "A computational chemistry library in clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :jvm-opts ["-Xmx1g -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m"])
 
