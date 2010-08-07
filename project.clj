12162414c37aea3eded156f3f2d3505831ef16f9
Cyrus Harmon <sly@cassis.local>
2010-08-06 18:31:51 -0700

    use clojure (and contrib) RC2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 
 (defproject chemiclj "0.0.0"
   :description "A computational chemistry library in clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
+  :dependencies [[org.clojure/clojure "1.2.0-RC2"]
+                 [org.clojure/clojure-contrib "1.2.0-RC2"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :jvm-opts ["-Xmx1g -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m"])
 
