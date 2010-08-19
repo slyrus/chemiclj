79cbc0a032cc2bfcb56c442750963e8e0e50e79f
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-08-19 10:30:41 -0700

    update project.clj to use clojure and clojure-contrib 1.2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 
 (defproject chemiclj "0.0.1"
   :description "A computational chemistry library in clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-RC3"]
-                 [org.clojure/clojure-contrib "1.2.0-RC3"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
